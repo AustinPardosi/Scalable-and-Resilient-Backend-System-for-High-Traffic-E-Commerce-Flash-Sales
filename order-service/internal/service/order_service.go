5dc329ec9453110d880169c12bac1867f427f45e
Austin Gabriel Pardosi <gabrielpardosi26@gmail.com>
2025-07-29 16:46:36 +0700

    feat: add order

---
+++ order-service/internal/service/order_service.go
@@ -0,0 +1,34 @@
+package service
+
+import "order-service/internal/entity"
+
+type OrderService struct {
+
+}
+
+// NewOrderService creates a new order service
+func NewOrderService() *OrderService {
+	return &OrderService{}
+}
+
+// CreateOrder creates a new order
+func (s *OrderService) CreateOrder(order *entity.Order) (*entity.Order, error {
+	order.ID = 1
+	order.Status = "created"
+	order.Total = 100.0
+	return order, nil
+})
+
+// UpdateOrder updates an existing order
+func (s *OrderService) UpdateOrder(order *entity.Order) (*entity.Order, error) {
+	return order, nil
+}
+
+// CancelOrder cancels an existing order
+func (s *OrderService) CancelOrder(id int) (*entity.Order, error) {
+	order := &entity.Order{
+		ID: id, 
+		Status: "cancelled",
+	}
+	return order, nil
+}
